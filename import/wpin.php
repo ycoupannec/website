788a8fd53328fe973563e04efee17254cfd5dedd
ChrisWebbNZ <espiex@gmail.com>
2016-03-29 21:28:07 +1300

    Updates to creating terms, collections

--- import/wpin.php
+++ import/wpin.php
@@ -7,28 +7,13 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-
-/*
-	if(!isset($_GET['mode'])) {
-		die('need a mode');
-	}
-
-	$mode = $_GET['mode'];
-*/
-
-	
-	
 	//still_image
 	//$mode = 'still_image';
-	//$fgid = 37072; //fieldgroup id	
-	
-
+	//$fgid = 37072; //fieldgroup id
 	
 	//video
 	//$mode = 'video';
-	//$fgid = 35615; //fieldgroup id	
-	
-
+	//$fgid = 35615; //fieldgroup id
 	
 	//person
 	//$mode = 'person';
@@ -39,170 +24,224 @@ error_reporting(E_ALL);
 	//$fgid = '51154';//fieldgroup id
 
 	//text
-	$mode = 'text';
-	$fgid = '51186';
+	//$mode = 'text';
+	//$fgid = '51186';
+
 	
+	$modes = array(
+		'collections' => 35640,//$mode => $fgid
+		'still_image' => 37072, 
+		'video' => 35615,
+		'person' => 36254,
+		'audio' => 51154,
+		'text' => 51186,		
+	);
 
-
-	$fields = acf_get_fields_by_id($fgid);
-
-	switch($mode) {
-
-		//nodes
-		case 'still_image':
-		case 'person':
-		case 'text':
-		case 'video':
-		case 'audio':
-
-			$json = file_get_contents('http://new.knowledgebank.org.nz/import/drupalout.php?mode=' . $mode);
-			
-			$nodes = json_decode($json,true);
-
-			$templatepath = '/webs/new/import/node.php';
-
-			if(!empty($nodes) && file_exists($templatepath)) {
-
-				$count = 0;
-
-				$limit = 0;
+	foreach($modes as $mode => $fgid) :
 		
-				foreach($nodes as $node) :
+		//Get all the fields on an ACF field group
+		$fields = acf_get_fields_by_id($fgid);
 
-					//check if this already exists
-					$checkpost = get_post($node['nid']);					
+		switch($mode) {
 
-					if(!empty($checkpost) && $checkpost != null) { //already exists, move on without importing
-						echo $node['nid'] . " already exists, moving on\n";
+			//nodes
+			case 'still_image':
+			case 'person':
+			case 'text':
+			case 'video':
+			case 'audio':
+
+				//have to get content from another script, can't bootstrap both WP and Drupal in this one
+				//json is as good as anything for the job
+				$json = file_get_contents('http://new.knowledgebank.org.nz/import/drupalout.php?mode=' . $mode); 
+				
+				$nodes = json_decode($json,true);//true = return as A_ARRAY
+
+				$templatepath = '/webs/new/import/node.php';
+
+				if(!empty($nodes) && file_exists($templatepath)) {
+
+					$count = 0;
+
+					$limit = 0;
+			
+					foreach($nodes as $node) :
+
+						//check if this already exists
+						$checkpost = get_post($node['nid']);					
+
+						if(!empty($checkpost) && $checkpost != null) { //already exists, move on without importing
+							echo $node['nid'] . " already exists, moving on\n";
+							continue;
+						}
+
+						//else... do the import with the template
+
+						/********************************************/
+
+						include($templatepath);
+
+						/*********************************************/		
+
+
+						$count+=1;	
+
+						if($limit > 0 && $count == $limit) break;
+
+					endforeach;
+
+
+				}
+				else {
+
+					echo "$nodes is empty or no template found\n";
+					exit;
+
+				}
+
+				echo "Did $count nodes";
+
+
+			break;
+
+			case 'collections': //does both collections and series from drupal
+
+				$json = file_get_contents('http://new.knowledgebank.org.nz/import/drupalout.php?mode=collections');
+
+				$terms = json_decode($json,true);
+
+				$json2 = file_get_contents('http://new.knowledgebank.org.nz/import/drupalout.php?mode=series');
+
+				$terms2 = json_decode($json2,true);
+
+				$d_collections_unsorted = array_merge($terms,$terms2);
+
+				$d_collections = array();
+
+				foreach($d_collections_unsorted as $term) {
+					$d_collections[$term['tid']] = $term;
+				}
+
+				//print_r($d_collections);
+
+				echo count($d_collections) . " collections in Drupal\n";
+/*
+				$wp_collections_unsorted = get_terms('collections',array('hide_empty' => false));
+
+				$wp_collections = array();
+
+				foreach($wp_collections_unsorted as $term) {
+					$wp_collections[$term->term_id] = $term;
+				}
+*/
+
+				foreach($d_collections as $t) {
+
+					$tid = $t['tid'];
+
+					//if(get_term($tid,'collections')) {
+					//	echo 'Term ' . $tid . " already exists, moving on\n";
+					//	continue;
+					//}
+
+					//else get on with inserting the term
+
+					$wpdb->query('INSERT INTO `wp_terms` values ('  . $t["tid"] . ',"' . $t["name"] . '","' . $t["tid"] . '",0) ON DUPLICATE KEY UPDATE term_id = ' . $t["tid"] . ', name = "' . $t['name'] . '", slug = "' . $t['tid'] . '"');	
+					
+					$parent = 0;
+
+					if(isset($t['field_collections']['und'][0]['tid'])){
+						$parent = $t['field_collections']['und'][0]['tid'];		
+					}
+
+					$wpdb->query('INSERT INTO `wp_term_taxonomy` (term_id, taxonomy, parent) VALUES ("' . $t["tid"] . '", "' . $taxonomy . '", "' . $parent . '") ON DUPLICATE KEY UPDATE term_id = ' . $t['tid'] . ', taxonomy = "' . $taxonomy . '", parent = ' . $parent);
+				
+
+					echo 'Inserted term ' . $t["tid"] . ' (' . $t['name'] . ')' . "\n";
+
+					$filefield = 'field_donor_form';
+					if(isset($t[$filefield]['und']['0']['uri']) && !get_field('donor_form','collections_' . $tid)) {
+						$file_url = str_replace('public://','/webs/hbda/sites/default/files/', $t[$filefield]['und']['0']['uri']);
+
+						$fileid = kb_fetch_media($file_url,$tid,'/collections/'.$tid.'/');
+
+						if($fileid) {
+
+							update_field('field_56b452c658411',$fileid,'collections_' . $tid);
+							echo 'File ' . $fileid . ' attached to ' . $tid . "\n";
+
+						}
+						
+					}
+
+
+					foreach($fields as $field) {
+
+						//simple text fields
+
+						if(in_array($field['type'], array('text','radio','wysiwyg','true_false','select')) && isset($t['field_' . $field['name']]['und'][0]['value'])) {
+
+							echo "Doing field " . 'field_' . $field['name'] . "\n";
+
+							update_field($field['key'], $t['field_' . $field['name']]['und'][0]['value'], 'collections_' . $tid);
+							echo "Added value " .  $t['field_' . $field['name']]['und'][0]['value'] . ' to ' . $field['name'] . ' (' . $field['key'] . ')' . "\n";
+							unset($t['field_' . $field['name']]);
+						}
+						elseif( isset($t['field_' . $field['name']]) ){
+							unset($t['field_' . $field['name']]);
+						}
+
+
+					}
+
+
+					echo 'Updated term ' . $tid . ' (' . $t['name'] . ')' . "\n";
+
+					echo 'Left in d_term: ' . print_r($t,true) . "\n";	
+
+				}			
+
+			break;
+
+			case 'tags':
+			case 'subjects':
+		
+				$json = file_get_contents('http://new.knowledgebank.org.nz/import/drupalout.php?mode=' . $mode);
+
+				$terms = json_decode($json,true);
+
+				foreach($terms as $t) {
+
+					$tid = $t['tid'];
+
+					if(get_term($tid,$mode)) {
+						echo 'Term ' . $tid . " already exists, moving on\n";
 						continue;
 					}
 
-					//else... do the import with the template
+					//else get on with inserting the term
 
-					/********************************************/
-
-					include($templatepath);
-
-					/*********************************************/		
-
-
-					$count+=1;	
-
-					if($limit > 0 && $count == $limit) break;
-
-				endforeach;
-
-
-			}
-			else {
-
-				echo "$nodes is empty or no template found\n";
-				exit;
-
-			}
-
-			echo "Did $count nodes";
-
-
-		break;
-
-		case 'collections': //does both collections and series from drupal
-
-			$json = file_get_contents('http://new.knowledgebank.org.nz/import/drupalout.php?mode=collections');
-
-			$terms = json_decode($json,true);
-
-			$json2 = file_get_contents('http://new.knowledgebank.org.nz/import/drupalout.php?mode=series');
-
-			$terms2 = json_decode($json2,true);
-
-			$terms = array_merge($terms,$terms2);
-
-			foreach($terms as $t) {
-
-				$tid = $t['tid'];
-
-				if(get_term($tid,'collections')) {
-					echo 'Term ' . $tid . " already exists, moving on\n";
-					continue;
-				}
-
-				//else get on with inserting the term
-
-				$wpdb->query('INSERT INTO `wp_terms` values ('  . $t["tid"] . ',"' . $t["name"] . '","' . $t["tid"] . '",0)');	
-				
-				$parent = 0;
-
-				if(isset($t['field_collections']['und'][0]['tid'])){
-					$parent = $t['field_collections']['und'][0]['tid'];		
-				}
-
-				$wpdb->query('INSERT INTO `wp_term_taxonomy` (term_id, taxonomy, parent) VALUES ("' . $t["tid"] . '", "collections", "' . $parent . '")');
-			
-
-				echo 'Inserted term ' . $t["tid"] . ' (' . $t['name'] . ')' . "\n";
-
-				$filefield = 'field_donor_form';
-				if(isset($t[$filefield]['und']['0']['uri'])) {
-					$file_url = str_replace('public://','/webs/hbda/sites/default/files/', $t[$filefield]['und']['0']['uri']);
-
-					$fileid = kb_fetch_media($file_url,$tid,'/collections/'.$tid.'/');
-
-					if($fileid) {
-
-						update_field('field_56b452c658411',$fileid,'collections_' . $tid);
-						echo 'File ' . $fileid . ' attached to ' . $tid . "\n";
-
-					}
+					$wpdb->query('INSERT INTO `wp_terms` values ('  . $t["tid"] . ',"' . $t["name"] . '","' . $t["tid"] . '",0)');	
 					
-				}
+					$parent = 0;
 
-
-				echo 'Updated term ' . $tid . ' (' . $t['name'] . ')' . "\n";
-
-			}			
-
-		break;
-
-		case 'tags':
-		case 'subjects':
-	
-			$json = file_get_contents('http://new.knowledgebank.org.nz/import/drupalout.php?mode=' . $mode);
-
-			$terms = json_decode($json,true);
-
-			foreach($terms as $t) {
-
-				$tid = $t['tid'];
-
-				if(get_term($tid,$mode)) {
-					echo 'Term ' . $tid . " already exists, moving on\n";
-					continue;
-				}
-
-				//else get on with inserting the term
-
-				$wpdb->query('INSERT INTO `wp_terms` values ('  . $t["tid"] . ',"' . $t["name"] . '","' . $t["tid"] . '",0)');	
+					$wpdb->query('INSERT INTO `wp_term_taxonomy` (term_id, taxonomy, parent) VALUES ("' . $t["tid"] . '", "' . $mode . '", "' . $parent . '")');
 				
-				$parent = 0;
+					echo 'Inserted term ' . $t["tid"] . ' (' . $t['name'] . ')' . "\n";
+					
+				}						
 
-				$wpdb->query('INSERT INTO `wp_term_taxonomy` (term_id, taxonomy, parent) VALUES ("' . $t["tid"] . '", "' . $mode . '", "' . $parent . '")');
+			break;		
+
 			
-				echo 'Inserted term ' . $t["tid"] . ' (' . $t['name'] . ')' . "\n";
-				
-			}						
 
-		break;		
 
 		
 
-
-	
-
-	}//switch
+		}//switch
 
 
-
+	endforeach;
 
 
 function kb_fetch_media($file_url, $nid, $dir) {
@@ -361,6 +400,8 @@ function acf_key($name, $subfield = null) {
 
 function kb_add_term($cid,$taxonomy) {
 
+	global $wpdb;
+
 	$json = file_get_contents('http://new.knowledgebank.org.nz/import/drupalout.php?mode=' . $taxonomy . '&tid=' . $cid);
 
 	$t = json_decode($json,true);
@@ -369,7 +410,7 @@ function kb_add_term($cid,$taxonomy) {
 
 		$tid = $t['tid'];
 
-		$wpdb->query('INSERT INTO `wp_terms` values ('  . $t["tid"] . ',"' . $t["name"] . '","' . $t["tid"] . '",0)');	
+		$wpdb->query('INSERT INTO `wp_terms` values ('  . $t["tid"] . ',"' . $t["name"] . '","' . $t["tid"] . '",0) ON DUPLICATE KEY UPDATE term_id = ' . $t["tid"] . ', name = "' . $t['name'] . '", slug = "' . $t['tid'] . '"');	
 		
 		$parent = 0;
 
@@ -377,7 +418,7 @@ function kb_add_term($cid,$taxonomy) {
 			$parent = $t['field_collections']['und'][0]['tid'];		
 		}
 
-		$wpdb->query('INSERT INTO `wp_term_taxonomy` (term_id, taxonomy, parent) VALUES ("' . $t["tid"] . '", $taxonomy, "' . $parent . '")');
+		$wpdb->query('INSERT INTO `wp_term_taxonomy` (term_id, taxonomy, parent) VALUES ("' . $t["tid"] . '", "' . $taxonomy . '", "' . $parent . '") ON DUPLICATE KEY UPDATE term_id = ' . $t['tid'] . ', taxonomy = "' . $taxonomy . '", parent = ' . $parent);
 		
 		echo 'Inserted term ' . $t["tid"] . ' (' . $t['name'] . ')' . "\n";
 
@@ -403,6 +444,7 @@ function kb_add_term($cid,$taxonomy) {
 	}
 
 }
+
 
 
 
