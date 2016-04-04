ca35c7e1270044f72ab545fb8cf260ed04a3bb5c
ChrisWebbNZ <espiex@gmail.com>
2016-04-04 18:15:26 +1200

    Fix ACF shenanigans on post previews

--- wp-content/themes/kb2/functions.php
+++ wp-content/themes/kb2/functions.php
@@ -432,5 +432,15 @@ add_action('init', 'change_permalinks');
 
 
 
+add_filter('_wp_post_revision_fields', 'add_field_debug_preview');
+function add_field_debug_preview($fields){
+   $fields["debug_preview"] = "debug_preview";
+   return $fields;
+}
+add_action( 'edit_form_after_title', 'add_input_debug_preview' );
+function add_input_debug_preview() {
+   echo '<input type="hidden" name="debug_preview" value="debug_preview">';
+}
+
 
 ?>
