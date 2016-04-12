4cc968229555c0c47fbe23486c9a8faefd8d1eaf
ChrisWebbNZ <espiex@gmail.com>
2016-04-12 21:15:35 +1200

    fix permalink

--- wp-content/themes/kb2/front-page.php
+++ wp-content/themes/kb2/front-page.php
@@ -10,7 +10,7 @@
 				foreach($landingpages as $page): ?>
 
 					<div class="grid-4">
-						<a href="<?php echo $page['landing_page']->ID; ?>"><img src="<?php echo $page['image']['sizes']['300w']; ?>" alt="<?php echo $page['image']['alt'] ?>" /></a>	
+						<a href="<?php echo get_permalink($page['landing_page']->ID); ?>"><img src="<?php echo $page['image']['sizes']['300w']; ?>" alt="<?php echo $page['image']['alt'] ?>" /></a>	
 						<h3><?php echo $page['landing_page']->post_title; ?></h3>
 						<p><?php echo $page['blurb']; ?></p>						
 					</div>
