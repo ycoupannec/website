c4343dccdd61f25ec64807390dc06728c0c6b0f6
ChrisWebbNZ <espiex@gmail.com>
2016-04-12 20:58:43 +1200

    Slightly easier way of doing home page tiles

--- wp-content/themes/kb2/front-page.php
+++ wp-content/themes/kb2/front-page.php
@@ -1,56 +1,26 @@
-<?php get_header(); ?>
+<?php get_header();  ?>
 
 <div class="grid-container">
-
 		
 		<?php
-			$images = get_field( 'images' ); ?>
-			<div class='grid-4'>
-			 <a href="<?php echo get_permalink(51366); ?>">
-			 <img src="<?php echo $images[0]['images_thumbnail']['sizes']['thumbnail']; ?>" alt="<?php echo $images[0]['images_thumbnail']['alt'] ?>" />
-			 <p><?php echo $images[0]['images_blurb']; ?></p>
-			 </a>
-			</div>
-	
+			$landingpages = get_field('content_landing_pages');
 
-		<?php 
-			$publications = get_field( 'publications' ); ?>
-			<div class='grid-4'>
-			<a href="<?php echo get_permalink(113036); ?>">
-				<img src="<?php echo $publications[0]['publications_image']['sizes']['thumbnail']; ?>" alt="<?php echo $publications[0]['publications_image']['alt'] ?>" />
-		 		<p><?php echo $publications[0]['publications_blurb']; ?></p>
-		 	</a>
-			</div>
+			if(!empty($landingpages)):
 
-		<?php 
-			$people = get_field('people'); ?>
-			<div class='grid-4'>
-			<a href="<?php echo get_permalink(51040); ?>">
-				<img src="<?php echo $people[0]['people_image']['sizes']['thumbnail']; ?>" alt="<?php echo $people[0]['people_image']['alt'] ?>" />
-		 		<p><?php echo $people[0]['people_blurb']; ?></p>
-		 	</a>
-			</div>
-			
-		</div><div class="grid-container">
+				foreach($landingpages as $page): ?>
 
-		<?php 
-			$films = get_field('films'); ?>
-			<div class='grid-4'>
-			<a href="<?php echo get_permalink(113038); ?>">
-				<img src="<?php echo $films[0]['films_image']['sizes']['thumbnail']; ?>" alt="<?php echo $films[0]['films_image']['alt'] ?>" />
-		 		<p><?php echo $films[0]['films_blurb']; ?></p>
-		 	</a>
-			</div>
-		
-		<?php 
-			$audio = get_field('audio'); ?>
-			
-			<div class='grid-4'>
-				<img src="<?php echo $audio[0]['audio_image']['sizes']['thumbnail']; ?>" alt="<?php echo $audio[0]['audio_image']['alt'] ?>" />
-		 		<p><?php echo $audio[0]['audio_blurb']; ?></p>
-			</div>
-			
+					<div class="grid-4">
+						<a href="<?php echo $page['landing_page']->ID; ?>"><img src="<?php echo $page['image']['sizes']['300w']; ?>" alt="<?php echo $page['image']['alt'] ?>" /></a>	
+						<h3><?php echo $page['landing_page']->post_title; ?></h3>
+						<p><?php echo $page['blurb']; ?></p>						
+					</div>
 
+		<?php
+				endforeach;
+
+			endif;
+
+		?>
 
 </div>
 
