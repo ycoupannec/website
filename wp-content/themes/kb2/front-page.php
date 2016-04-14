841a4d0e8b1534659840b796c08a75f5b759c576
ChrisWebbNZ <espiex@gmail.com>
2016-04-14 20:03:06 +1200

    Add flexslider

--- wp-content/themes/kb2/front-page.php
+++ wp-content/themes/kb2/front-page.php
@@ -1,26 +1,62 @@
 <?php get_header();  ?>
 
+
+
+<?php $slides = get_field('slider'); ?>
+
+<?php if(!empty($slides)): ?>
+
+	<div class="flexslider">
+
+		<ul class="slides">
+
+		<?php foreach($slides as $slide): ?>
+
+			<li>
+
+				<?php
+					//get our href - it's either a permalink from a post object or a string from a text field, depending on what link_type is set to
+					$link = ( $slide['link_type'] == 'internal' ? get_permalink($slide['internal']->ID) : $slide['external'] );
+				?>
+
+				<a href="<?php echo $link; ?>" <?php if($slide['new_window'] == 1) echo 'target="_blank"'; ?>>
+					<img src="<?php echo $slide['image']['sizes']['1200w']; ?>" alt="<?php echo $slide['image']['alt'] ?>" />
+				</a>
+
+			</li>
+
+		<?php endforeach; ?>
+
+		</ul>
+
+	</div>
+
+<?php endif; ?>
+
+
+
+
 <div class="grid-container">
 		
-		<?php
-			$landingpages = get_field('content_landing_pages');
+	<?php
+		$landingpages = get_field('content_landing_pages');
 
-			if(!empty($landingpages)):
+		if(!empty($landingpages)):
 
-				foreach($landingpages as $page): ?>
+			foreach($landingpages as $page): ?>
 
-					<div class="grid-4">
-						<a href="<?php echo get_permalink($page['landing_page']->ID); ?>"><img src="<?php echo $page['image']['sizes']['300w']; ?>" alt="<?php echo $page['image']['alt'] ?>" /></a>	
-						<h3><?php echo $page['landing_page']->post_title; ?></h3>
-						<p><?php echo $page['blurb']; ?></p>						
-					</div>
+				<div class="grid-4">
+					<a href="<?php echo get_permalink($page['landing_page']->ID); ?>"><img src="<?php echo $page['image']['sizes']['300w']; ?>" alt="<?php echo $page['image']['alt'] ?>" /></a>	
+					<h3><?php echo $page['landing_page']->post_title; ?></h3>
+					<p><?php echo $page['blurb']; ?></p>						
+				</div>
 
-		<?php
-				endforeach;
+	<?php
+			endforeach;
 
-			endif;
+		endif;
 
-		?>
+	?>
 
 </div>
 
