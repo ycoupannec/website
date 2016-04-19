ed27495bbdaaf885c4071148f6955f6e9e35fdee
Gabriel Ward <gabrielward@gmail.com>
2016-04-19 21:39:19 +1200

    added code to page-maps.php

--- wp-content/themes/kb2/page-maps.php
+++ wp-content/themes/kb2/page-maps.php
@@ -7,32 +7,17 @@
 	'tax_query' => array(
 		array(
 			'taxonomy' => 'subject',
-			'field'    => 'subjects',
-			'terms'    => 'Maps',
+			'field'    => 'term_id',
+			'terms'    => array( 765 ),
+			'operator' => 'IN',
 		),
 	),
 );
 
-  $posts = get_posts($args);
-  print_r($posts);
+
+$maps = get_posts($args);
+
+print_r($maps);
 ?>
-
-<?php
-
-		$maps = $wpdb->get_results(
-			"
-			SELECT DISTINCT t.term_id, t.name 
-			FROM `wp_term_taxonomy` tt 
-			LEFT JOIN `wp_term_relationships` tr ON tt.term_taxonomy_id = tr.term_taxonomy_id 
-			LEFT JOIN `wp_posts` p ON tr.object_id = p.ID 
-			LEFT JOIN `wp_terms` t ON t.term_id = tt.term_id 
-			WHERE p.post_type = 'still_image' 
-			AND tt.taxonomy = 'subject'
-			AND tt.term_taxonomy_name = 'Maps'
-			ORDER BY tt.term_id ASC
-			"
-		);
-
-		print_r($maps); ?>
 
 <?php get_footer(); ?>
