70f6c831170a859043f10a0d7d0252bfe7506612
Gabriel Ward <gabrielward@gmail.com>
2016-04-20 17:50:45 +1200

    added page-photo_news.php

--- wp-content/themes/kb2/page-maps.php
+++ wp-content/themes/kb2/page-maps.php
@@ -1,23 +1,58 @@
 <?php get_header(); ?>
-<div class="pageTitles">
-	<h1><?php the_title(); ?></h1>
-</div>
-<?php $args = array(
-	'post_type' => 'still_image',
-	'tax_query' => array(
-		array(
-			'taxonomy' => 'subject',
-			'field'    => 'term_id',
-			'terms'    => array( 765 ),
-			'operator' => 'IN',
+	<div class="pageTitles">
+		<h1><?php the_title(); ?></h1>
+	</div>
+	<?php $args = array(
+		'post_type' => 'still_image',
+		'tax_query' => array(
+			array(
+				'taxonomy' => 'subject',
+				'field'    => 'term_id',
+				'terms'    => array( 765 ),
+			),
 		),
-	),
-);
+	);
 
+	$maps = new WP_Query($args); ?>
 
-$maps = get_posts($args);
+	<div class='grid-container'>
+		<?php if ( $maps->have_posts() ) : while ( $maps->have_posts() ) : $maps->the_post(); ?>
+			<div class='grid-4'>
+			<?php $image = get_field('images'); ?>
+				<a href="<?php echo get_permalink(); ?>">
+					<img src="<?php echo $image[0]['image']['sizes']['thumbnail']; ?>">
+					<?php the_title(); ?>
+				</a>
+			</div>
+			<?php $blog_count = $maps->current_post+1; ?>
+     		<?php if ( $blog_count % 3 == 0 && $blog_count != $maps->post_count) : ?>
+        		</div><div class='grid-container group'>
+      		<?php endif; ?>
+		<?php endwhile; endif; ?>
+	</div>
 
-print_r($maps);
-?>
+	<?php 
+	//this code uses get_posts() and but only returns 5 posts, 
+	//when it should return 9
+	$args = array(
+		'post_type' => 'still_image',
+		'tax_query' => array(
+			array(
+				'taxonomy' => 'subject',
+				'field'    => 'term_id',
+				'terms'    => array( 765 ),
+			),
+		),
+	);
+
+	$maps = get_posts($args); ?>
+
+	<div class='grid-container'>
+		<?php foreach($maps as $map) : ?>
+			<div class='grid-4'>
+				<?php echo $map->post_title; ?>
+			</div>
+		<?php endforeach; ?>
+	</div>
 
 <?php get_footer(); ?>
