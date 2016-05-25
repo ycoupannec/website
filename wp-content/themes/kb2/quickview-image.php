fd96b234fe4d080addaa0742e8cfcf82fb996f34
ChrisWebbNZ <espiex@gmail.com>
2016-05-25 17:42:30 +1200

    Add still image quickview template

---
+++ wp-content/themes/kb2/quickview-image.php
@@ -0,0 +1,15 @@
+<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+<div>
+	<?php $images = get_field('images'); ?>
+
+		<?php if(!empty($images)): ?>
+			<div class="images">
+			<?php foreach($images as $image): ?>
+
+				<img src="<?php echo $image['image']['sizes']['large']; ?>" class="gallery-image" />
+
+			<?php endforeach; ?>
+			</div>
+		<?php endif; ?>
+</div>
+<?php endwhile; endif; ?>
