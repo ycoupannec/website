3179c3dd70850f7af8d1473683ee7717d0660b02
ChrisWebbNZ <espiex@gmail.com>
2016-06-01 20:20:30 +1200

    Tweaks to tiles

--- wp-content/themes/kb2/page-photonews.php
+++ wp-content/themes/kb2/page-photonews.php
@@ -1,19 +1,15 @@
-<?php  get_header(); ?>
+<?php get_header(); ?>
+
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 		<div class="pageTitles">
-			<h1><?php the_title(); ?></h1>
-			<?php 
+			<h1><?php the_title(); ?></h1>		
 
-				if (have_posts()) : while (have_posts()) : the_post();
+			<?php the_content(); ?>
 
-					the_content();
-
-				endwhile; endif; 
-
-			?>			
 		</div>
 		
-		<section class="tiles years">
+		<section class="tiles tiles-4 years">
 
 			<?php 
 
@@ -23,9 +19,11 @@
 				    'hide_empty' => false,
 				) );
 
-				if(!empty($years)) :
+			?>
 
-					foreach($years as $year): ?>
+			<?php if(!empty($years)) : ?>
+
+				<?php foreach($years as $year): ?>
 
 						<div <?php post_class('tile'); ?>>
 
@@ -50,67 +48,12 @@
 							
 						</div>
 
-			<?php
-					endforeach;
-			?>
+				<?php endforeach; ?>
 
 			<?php endif; ?>
 
-
-
 		</section>
 
-
-
-
-		<section class="tiles">
-
-
-		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
-
-			
-
-			<?php 
-
-				//here we include a standard 'tile' template for each of our media types - tile-audio.php, tile-video.php
-				//They all have the same structure, but include the different players for audio or video, or show a still image etc
-				//We can use these tile templates wherever we want records shown as tiles, anywhere on the site.
-
-				get_template_part('tile', $post->post_type);
-
-			?>	
-
-			
-
-		<?php endwhile; ?>
-
-
-		<?php 
-
-			// Previous/next page navigation.
-			the_posts_pagination( array(
-				'prev_text'          => 'Previous page',
-				'next_text'          => 'Next Page',
-				'before_page_number' => '',
-			) );
-
-		?>
-
-
-		<?php else: ?>
-
-			<!-- article -->
-			<article>
-
-				<h2><?php _e( 'Sorry, nothing to display.', 'kb2' ); ?></h2>
-
-			</article>
-			<!-- /article -->
-
-		<?php endif; ?>
-
-		</section>
-
-
+<?php endwhile; endif; ?>
 
 <?php get_footer(); ?>
--- wp-content/themes/kb2/taxonomy.php
+++ wp-content/themes/kb2/taxonomy.php
@@ -1,27 +1,11 @@
 <?php  get_header(); ?>
 
 
-		<section class="tiles">
-
+		<section class="tiles tiles-4">
 
 		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
-
-			
-
-			<?php 
-
-				//here we include a standard 'tile' template for each of our media types - tile-audio.php, tile-video.php
-				//They all have the same structure, but include the different players for audio or video, or show a still image etc
-				//We can use these tile templates wherever we want records shown as tiles, anywhere on the site.
-
-				get_template_part('tile', $post->post_type);
-
-			?>	
-
-			
-
+			<?php get_template_part('tile', $post->post_type); ?>		
 		<?php endwhile; ?>
-
 
 		<?php 
 
@@ -34,9 +18,7 @@
 
 		?>
 
-
 		<?php else: ?>
-
 			<!-- article -->
 			<article>
 
@@ -44,7 +26,6 @@
 
 			</article>
 			<!-- /article -->
-
 		<?php endif; ?>
 
 		</section>
